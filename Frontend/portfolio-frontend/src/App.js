8525da6dd88eb223b6c43a5d449894d7596d41ba
dogarthecreator <134731805+dogarthecreator@users.noreply.github.com>
2024-02-04 16:51:13 +0500

    Adding Client Review Section

--- Frontend/portfolio-frontend/src/App.js
+++ Frontend/portfolio-frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Screen1 from "./Screens/Screen1";
+import ClientScreen from "./Screens/ClientScreen";
 
 function App() {
     return (
         <>
             <Screen1 />
+            <ClientScreen />
         </>
     );
 }
---
+++ Frontend/portfolio-frontend/src/Screens/ClientScreen.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Review from "../Components/Review";
+
+const ClientSection = () => {
+    const reviews = [
+        {
+            reviewText:
+                "This team managed to deliver excellent results on time, exceeding our expectations!",
+            reviewerName: "Alex Johnson",
+            reviewerCompany: "CEO at Entaroads",
+            rating: 5,
+            reviewerImage: "https://source.unsplash.com/random/200x200?sig=1",
+        },
+        {
+            reviewText:
+                "Their attention to detail and creative approach was exactly what we needed. Highly recommend!",
+            reviewerName: "Morgan Smith",
+            reviewerCompany: "Marketing Director at Creativia",
+            rating: 4,
+            reviewerImage: "https://source.unsplash.com/random/200x200?sig=2",
+        },
+        {
+            reviewText:
+                "While the project was completed with high standards, communication could have been better.",
+            reviewerName: "Chris Doe",
+            reviewerCompany: "Product Manager at TechCorp",
+            rating: 3,
+            reviewerImage: "https://source.unsplash.com/random/200x200?sig=3",ß
+        },
+    ];
+
+    return (
+        <section className="pt-8">
+            <div className=" border-solid border-b-2 pb-12">
+                <h2 className="text-4xl font-semibold text-center mb-6">
+                    Clients
+                </h2>
+                <div className="flex flex-wrap justify-center">
+                    {reviews.map((review, index) => (
+                        <Review
+                            key={index}
+                            reviewText={review.reviewText}
+                            reviewerName={review.reviewerName}
+                            reviewerCompany={review.reviewerCompany}
+                            rating={review.rating}
+                            reviewerImage={review.reviewerImage}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ClientSection;
