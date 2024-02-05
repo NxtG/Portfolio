cf66fa30b137f6d9a0b2e4d1bb883106f8afe21d
Rafay575 <111084742+Rafay575@users.noreply.github.com>
2024-02-05 18:31:36 +0500

    Footer is added

--- Frontend/portfolio-frontend/src/App.js
+++ Frontend/portfolio-frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Screen1 from "./Screens/Screen1";
 import ClientScreen from "./Screens/ClientScreen";
+import Footer from "./Components/Footer";
 
 function App() {
     return (
         <>
             <Screen1 />
             <ClientScreen />
+            <Footer />
         </>
     );
 }
